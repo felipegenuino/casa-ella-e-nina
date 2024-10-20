0b7da2b3a6cbc6aa3bb5f706de3dd31886248142
Felipe Genuino <felipegenuino@MacBook-Air-de-Felipe.local>
2024-10-20 05:29:28 -0300

    update acessibilidade

--- src/app/components/HighlightsSection/index.js
+++ src/app/components/HighlightsSection/index.js
@@ -1,7 +1,7 @@
 // src/app/components/HighlightsSection/HighlightsSection.js
 "use client";
 
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Modal from 'react-modal';
 
 // import Swiper core and required modules
@@ -25,13 +25,13 @@ const galleries = [
     thumb: '/midias/gallery/thumb-1.jpg',
     title: 'Sobre as Casas',
     media: [
-      { type: 'image', url: '/midias/gallery/image-3.jpg' },
-      { type: 'video', url: '/midias/gallery/video-1.mp4' },
-      { type: 'image', url: '/midias/gallery/image-1.jpg' },
-      { type: 'video', url: '/midias/gallery/video-2.mp4' },
-      { type: 'image', url: '/midias/gallery/image-5.jpg' },
-      { type: 'image', url: '/midias/gallery/image-6.jpg' },
-      { type: 'image', url: '/midias/gallery/image-7.jpg' },
+      { type: 'image', url: '/midias/gallery/image-3.jpg', description: 'Tour virtual pela casa 1'  },
+      { type: 'video', url: '/midias/gallery/video-1.mp4', description: 'Aria label'  },
+      { type: 'image', url: '/midias/gallery/image-1.jpg', description: 'Aria label'  },
+      { type: 'video', url: '/midias/gallery/video-2.mp4', description: 'Aria label'  },
+      { type: 'image', url: '/midias/gallery/image-5.jpg', description: 'Aria label'  },
+      { type: 'image', url: '/midias/gallery/image-6.jpg', description: 'Aria label'  },
+      { type: 'image', url: '/midias/gallery/image-7.jpg', description: 'Aria label'  },
     ],
   },
   {
@@ -39,108 +39,130 @@ const galleries = [
     thumb: '/midias/gallery/thumb-2.jpg',
     title: 'Experiência na Praia',
     media: [
-        { type: 'image', url: '/midias/gallery/image-2.jpg' },
-      { type: 'video', url: '/midias/gallery/video-3.mp4' },
-      { type: 'image', url: '/midias/gallery/image-2.jpg' },
-      { type: 'video', url: '/midias/gallery/video-4.mp4' },
+      { type: 'image', url: '/midias/gallery/image-2.jpg', description: 'Aria label'  },
+      { type: 'video', url: '/midias/gallery/video-3.mp4', description: 'Aria label'  },
+      { type: 'image', url: '/midias/gallery/image-2.jpg', description: 'Aria label'  },
+      { type: 'video', url: '/midias/gallery/video-4.mp4', description: 'Aria label'  },
     ],
   },
 ];
-
 const HighlightsSection = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [activeGallery, setActiveGallery] = useState(null);
-  const [activeMediaIndex, setActiveMediaIndex] = useState(0);
-  const [isMuted, setIsMuted] = useState(true);
-
-  const toggleMute = () => {
-    setIsMuted(!isMuted);
-  };
-
-  const openModal = (galleryIndex) => {
-    setActiveGallery(galleries[galleryIndex]);
-    setActiveMediaIndex(0);
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-    setActiveGallery(null);
-  };
-
-  return (
-    <section>
-      <h2>Momentos Especiais</h2>
-      <ul className={styles['highlights-list']}>
-        {galleries.map((gallery, index) => (
-          <li key={gallery.id} className={styles['highlight-item']}>
-            <button onClick={() => openModal(index)}>
-              <img src={gallery.thumb} alt={gallery.title} />
-              <p>{gallery.title}</p>
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      {activeGallery && (
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          className={styles.modal}
-          overlayClassName={styles.overlay}
-        >
-         <div className={styles['modal-header-buttons']}>
-         <button onClick={closeModal} className={styles['close-button']}>
-            Fechar
-          </button>
-
-           {/* <button onClick={toggleMute} className={styles['mute-button']}>
-                          {isMuted ? 'Ativar Som' : 'Desativar Som'}
-                        </button>   */}
-
-         </div>
-          <Swiper
-            direction="vertical"
-            slidesPerView={1}
-            onSlideChange={(swiper) => setActiveMediaIndex(swiper.activeIndex)}
-            className={styles['swiper-container']}
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            navigation
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [activeGallery, setActiveGallery] = useState(null);
+    const [activeMediaIndex, setActiveMediaIndex] = useState(0);
+    const [isMuted, setIsMuted] = useState(true);
+    const closeModalButtonRef = useRef(null);
+  
+    const toggleMute = () => {
+      setIsMuted(!isMuted);
+    };
+  
+    const openModal = (galleryIndex) => {
+      setActiveGallery(galleries[galleryIndex]);
+      setActiveMediaIndex(0);
+      setModalIsOpen(true);
+    };
+  
+    const closeModal = () => {
+      setModalIsOpen(false);
+      setActiveGallery(null);
+    };
+  
+    useEffect(() => {
+      // Focar no botão de fechar quando o modal abrir
+      if (modalIsOpen && closeModalButtonRef.current) {
+        closeModalButtonRef.current.focus();
+      }
+    }, [modalIsOpen]);
+  
+    return (
+      <section>
+        <h2>Momentos Especiais</h2>
+        <ul className={styles['highlights-list']}>
+          {galleries.map((gallery, index) => (
+            <li key={gallery.id} className={styles['highlight-item']}>
+              <button
+                onClick={() => openModal(index)}
+                role="button"
+                aria-expanded={modalIsOpen}
+                aria-label={`Ver galeria de ${gallery.title}`}
+              >
+                <img src={gallery.thumb} alt={gallery.title} />
+                <p>{gallery.title}</p>
+              </button>
+            </li>
+          ))}
+        </ul>
+  
+        {activeGallery && (
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            className={styles.modal}
+            overlayClassName={styles.overlay}
+            ariaHideApp={false}
+            contentLabel={`Galeria de ${activeGallery.title}`}
           >
-            {activeGallery.media.map((mediaItem, index) => (
-              <SwiperSlide key={index}>
-                <div className={styles['media-wrapper']}>
-                  {activeMediaIndex === index && (
-                    mediaItem.type === 'video' ? (
-                      <div className={styles['video-container']}>
-                        <video
-                          src={mediaItem.url}
-                        //   controls
-                          muted={!isMuted}
-                          autoPlay
-                          loop
-                          className={`${styles['media-player']} ${styles['local-video']}`}
-                        ></video>
-                       
-                      </div>
-                    ) : (
-                      <img
-                        src={mediaItem.url}
-                        alt={`Image ${index + 1} of ${activeGallery.title}`}
-                        className={styles['media-player']}
-                      />
-                    )
-                  )}
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </Modal>
-      )}
-    </section>
-  );
-};
+            <button
+              onClick={closeModal}
+              className={styles['close-button']}
+              ref={closeModalButtonRef}
+              aria-label="Fechar galeria"
+            >
+              Fechar
+            </button>
+  
+            <Swiper
+              direction="vertical"
+              slidesPerView={1}
+              onSlideChange={(swiper) => setActiveMediaIndex(swiper.activeIndex)}
+              className={styles['swiper-container']}
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              navigation
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+            >
+              {activeGallery.media.map((mediaItem, index) => (
+                <SwiperSlide key={index}>
+                  <div className={styles['media-wrapper']}>
+                    {activeMediaIndex === index && (
+                      mediaItem.type === 'video' ? (
+                        <div className={styles['video-container']}>
+                            <button
+                            onClick={toggleMute}
+                            className={styles['mute-button']}
+                            aria-label={isMuted ? 'Ativar som' : 'Desativar som'}
+                          >
+                            {!isMuted ? 'Ativar Som' : 'Desativar Som'}
+                          </button>
 
-export default HighlightsSection;
+                          <video
+                            src={mediaItem.url}
+                            //controls
+                            muted={!isMuted}
+                            autoPlay
+                            loop
+                            className={`${styles['media-player']} ${styles['local-video']}`}
+                            aria-label={mediaItem.description}
+                          ></video>
+                          
+                        </div>
+                      ) : (
+                        <img
+                          src={mediaItem.url}
+                          alt={mediaItem.description}
+                          className={styles['media-player']}
+                        />
+                      )
+                    )}
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </Modal>
+        )}
+      </section>
+    );
+  };
+  
+  export default HighlightsSection;
