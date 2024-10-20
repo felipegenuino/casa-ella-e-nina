9e0162cb4746d6505f26c6326486508a5ec8db51
Felipe Genuino <felipegenuino@MacBook-Air-de-Felipe.local>
2024-10-20 05:20:19 -0300

    update swipe

--- src/app/components/HighlightsSection/index.js
+++ src/app/components/HighlightsSection/index.js
@@ -5,60 +5,61 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 
 // import Swiper core and required modules
-import {  Navigation, Pagination, Scrollbar, A11y  } from 'swiper/modules';
-import { Swiper, SwiperSlide} from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-import SwiperCore from 'swiper'; 
-
-
+import SwiperCore from 'swiper';
 import styles from './index.module.css';
-
 
 SwiperCore.use([Pagination, Navigation]);
 
+// Atualizamos a estrutura do array para incluir vídeos e imagens.
 const galleries = [
-    {
-      id: 1,
-      thumb: '/midias/gallery/thumb-1.jpg',
-      title: 'Sobre as Casas',
-      videos: [
-        '/midias/gallery/thumb-1.mp4',
-        '/midias/gallery/thumb-2.mp4',
-        '/midias/gallery/thumb-3.mp4',
-      ],
-    },
-    {
-      id: 2,
-      thumb: '/midias/gallery/thumb-2.jpg',
-      title: 'Experiência na Praia',
-      videos: [
-        '/midias/gallery/thumb-1.mp4',
-        '/midias/gallery/thumb-2.mp4',
-        '/midias/gallery/thumb-3.mp4',
-      ],
-    },
-  ];
-
-
-// Definindo a função isYouTubeVideo
-const isYouTubeVideo = (videoUrl) => {
-    return videoUrl.includes('youtube.com') || videoUrl.includes('youtu.be');
-  };
+  {
+    id: 1,
+    thumb: '/midias/gallery/thumb-1.jpg',
+    title: 'Sobre as Casas',
+    media: [
+      { type: 'image', url: '/midias/gallery/image-3.jpg' },
+      { type: 'video', url: '/midias/gallery/video-1.mp4' },
+      { type: 'image', url: '/midias/gallery/image-1.jpg' },
+      { type: 'video', url: '/midias/gallery/video-2.mp4' },
+      { type: 'image', url: '/midias/gallery/image-5.jpg' },
+      { type: 'image', url: '/midias/gallery/image-6.jpg' },
+      { type: 'image', url: '/midias/gallery/image-7.jpg' },
+    ],
+  },
+  {
+    id: 2,
+    thumb: '/midias/gallery/thumb-2.jpg',
+    title: 'Experiência na Praia',
+    media: [
+        { type: 'image', url: '/midias/gallery/image-2.jpg' },
+      { type: 'video', url: '/midias/gallery/video-3.mp4' },
+      { type: 'image', url: '/midias/gallery/image-2.jpg' },
+      { type: 'video', url: '/midias/gallery/video-4.mp4' },
+    ],
+  },
+];
 
 const HighlightsSection = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [activeGallery, setActiveGallery] = useState(null);
-  const [activeVideoIndex, setActiveVideoIndex] = useState(0);
- 
+  const [activeMediaIndex, setActiveMediaIndex] = useState(0);
+  const [isMuted, setIsMuted] = useState(true);
+
+  const toggleMute = () => {
+    setIsMuted(!isMuted);
+  };
 
   const openModal = (galleryIndex) => {
     setActiveGallery(galleries[galleryIndex]);
-    setActiveVideoIndex(0);
+    setActiveMediaIndex(0);
     setModalIsOpen(true);
   };
 
@@ -88,42 +89,54 @@ const HighlightsSection = () => {
           className={styles.modal}
           overlayClassName={styles.overlay}
         >
-          <button onClick={closeModal} className={styles['close-button']}>Fechar</button>
+         <div className={styles['modal-header-buttons']}>
+         <button onClick={closeModal} className={styles['close-button']}>
+            Fechar
+          </button>
 
+           {/* <button onClick={toggleMute} className={styles['mute-button']}>
+                          {isMuted ? 'Ativar Som' : 'Desativar Som'}
+                        </button>   */}
 
-
+         </div>
           <Swiper
             direction="vertical"
             slidesPerView={1}
-            onSlideChange={(swiper) => setActiveVideoIndex(swiper.activeIndex)}
+            onSlideChange={(swiper) => setActiveMediaIndex(swiper.activeIndex)}
             className={styles['swiper-container']}
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             navigation
             pagination={{ clickable: true }}
             scrollbar={{ draggable: true }}
           >
-            {activeGallery.videos.map((videoUrl, index) => (
+            {activeGallery.media.map((mediaItem, index) => (
               <SwiperSlide key={index}>
-                <div className={styles['video-wrapper']}>
-                  {activeVideoIndex === index && (
-                    <div className={styles['video-container']}>
-                      <video
-                        src={videoUrl}
-                        //controls
-                        autoPlay
-                        loop
-                        className={`${styles['video-player']} ${styles['local-video']}`}
-                      ></video>
-                    </div>
+                <div className={styles['media-wrapper']}>
+                  {activeMediaIndex === index && (
+                    mediaItem.type === 'video' ? (
+                      <div className={styles['video-container']}>
+                        <video
+                          src={mediaItem.url}
+                        //   controls
+                          muted={!isMuted}
+                          autoPlay
+                          loop
+                          className={`${styles['media-player']} ${styles['local-video']}`}
+                        ></video>
+                       
+                      </div>
+                    ) : (
+                      <img
+                        src={mediaItem.url}
+                        alt={`Image ${index + 1} of ${activeGallery.title}`}
+                        className={styles['media-player']}
+                      />
+                    )
                   )}
                 </div>
               </SwiperSlide>
             ))}
           </Swiper>
-
-
-
-          
         </Modal>
       )}
     </section>
