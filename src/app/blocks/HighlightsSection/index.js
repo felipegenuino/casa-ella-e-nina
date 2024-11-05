1a5dc6955ae0bc3155367e3f2f333178a830f960
Felipe Genuino <design@felipegenuino.com>
2024-11-05 10:42:32 -0300

    update: on press space stop video

--- src/app/blocks/HighlightsSection/index.js
+++ src/app/blocks/HighlightsSection/index.js
@@ -1,10 +1,18 @@
 "use client";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 import { useState, useRef, useEffect } from "react";
 import Modal from "react-modal";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+
+import SwiperCore from "swiper";
 import Image from "next/image";
+
+SwiperCore.use([Pagination, Navigation]);
 
 // Atualizamos a estrutura do array para incluir vídeos e imagens.
 const galleries = [
@@ -291,7 +299,9 @@ const HighlightsSection = () => {
   const [activeGallery, setActiveGallery] = useState(null);
   const [activeMediaIndex, setActiveMediaIndex] = useState(0);
   const [isMuted, setIsMuted] = useState(false);
+  const closeModalButtonRef = useRef(null);
   const videoRef = useRef(null);
+  const swiperRef = useRef(null);
 
   const toggleMute = () => {
     setIsMuted(!isMuted);
@@ -311,41 +321,71 @@ const HighlightsSection = () => {
     setActiveGallery(null);
   };
 
+  const handleVideoPress = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (modalIsOpen && closeModalButtonRef.current) {
+      closeModalButtonRef.current.focus();
+    }
+  }, [modalIsOpen]);
+
   useEffect(() => {
     if (videoRef.current) {
-      const handlePlayPause = () => {
-        setIsMuted(videoRef.current.muted);
-      };
-
-      videoRef.current.addEventListener("play", handlePlayPause);
-      videoRef.current.addEventListener("pause", handlePlayPause);
-
-      return () => {
-        videoRef.current.removeEventListener("play", handlePlayPause);
-        videoRef.current.removeEventListener("pause", handlePlayPause);
-      };
+      videoRef.current.muted = isMuted;
     }
-  }, [videoRef.current]);
+  }, [activeMediaIndex, isMuted]);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (modalIsOpen) {
+        if (e.key === "ArrowRight" && swiperRef.current) {
+          swiperRef.current.swiper.slideNext();
+        } else if (e.key === "ArrowLeft" && swiperRef.current) {
+          swiperRef.current.swiper.slidePrev();
+        } else if (e.key === " " && videoRef.current) {
+          // Prevenir o comportamento padrão de rolar a página quando a tecla de espaço é pressionada
+          e.preventDefault();
+          handleVideoPress();
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [modalIsOpen]);
 
   return (
-    <div className="highlight-container mx-auto py-5 w-full bg-white">
+    <div className="highlight-container mx-auto py-5 bg-white">
       <div className="max-w-7xl px-0 lg:px-8 mx-auto relative rounded-xl overflow-auto">
         <div role="list" className="flex flex-nowrap gap-1 overflow-x-auto">
           {galleries.map((gallery, index) => (
             <div role="listitem" key={gallery.id} className="text-center px-3">
               <button
                 onClick={() => openModal(index)}
+                aria-expanded={modalIsOpen}
                 aria-label={`Ver galeria de ${gallery.title}`}
               >
-                <div className="ease-in duration-300 bg-white hover:bg-violet-600 rounded-full p-0 shadow-sm aspect-square">
+                <div className="bg-white hover:bg-violet-600 rounded-full shadow-sm aspect-square">
                   <img
-                    className="rounded-full min-w-24 min-h-24 aspect-square p-1 border-solid border-2 border-black-600"
+                    className="rounded-full min-w-24 min-h-24 p-1 border-2 border-black"
                     src={gallery.thumb}
                     alt={gallery.title}
                     aria-hidden="true"
                   />
                 </div>
-                <p className="text-xs my-2 font-medium">{gallery.title}</p>
+                <p aria-hidden="true" className="text-xs my-2 font-medium">
+                  {gallery.title}
+                </p>
               </button>
             </div>
           ))}
@@ -356,72 +396,75 @@ const HighlightsSection = () => {
         <Modal
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50"
           ariaHideApp={false}
-          closeTimeoutMS={200}
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-90"
+          overlayClassName="fixed inset-0 bg-black bg-opacity-90"
           contentLabel={`Galeria de ${activeGallery.title}`}
         >
-          <header>
+          <div className="relative w-full h-full flex items-center justify-center">
             <button
               onClick={closeModal}
               className="absolute top-4 right-4 text-white bg-gray-800 rounded-full p-2 z-50"
+              ref={closeModalButtonRef}
               aria-label="Fechar galeria"
             >
               Fechar
             </button>
-          </header>
 
-          <Swiper
-            direction="vertical"
-            slidesPerView={1}
-            onSlideChange={(swiper) => setActiveMediaIndex(swiper.activeIndex)}
-            className="w-full h-full"
-            navigation
-            pagination={{ clickable: true }}
-          >
-            {activeGallery.media.map((mediaItem, index) => (
-              <SwiperSlide key={index}>
-                <div className="flex items-center justify-center h-full relative">
-                  {activeMediaIndex === index &&
-                    (mediaItem.type === "video" ? (
-                      <div>
-                        <video
-                          ref={videoRef}
-                          src={mediaItem.url}
-                          autoPlay
-                          muted={isMuted}
-                          loop
-                          className="w-full h-auto"
-                          aria-label={mediaItem.description}
-                          onClick={() => {
-                            if (videoRef.current.paused) {
-                              videoRef.current.play();
-                            } else {
-                              videoRef.current.pause();
+            <Swiper
+              direction="horizontal"
+              slidesPerView={1}
+              onSlideChange={(swiper) => {
+                setActiveMediaIndex(swiper.activeIndex);
+                setIsMuted(false);
+              }}
+              className="w-full h-full flex items-center justify-center"
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              navigation
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+              ref={swiperRef}
+            >
+              {activeGallery.media.map((mediaItem, index) => (
+                <SwiperSlide key={index}>
+                  <div className="flex items-center justify-center h-full w-full">
+                    {activeMediaIndex === index &&
+                      (mediaItem.type === "video" ? (
+                        <div className="relative w-full h-full flex items-center justify-center">
+                          <video
+                            ref={videoRef}
+                            src={mediaItem.url}
+                            muted={isMuted}
+                            autoPlay
+                            loop
+                            className="max-h-[90vh] max-w-full object-contain"
+                            aria-label={mediaItem.description}
+                            onClick={handleVideoPress}
+                          ></video>
+                          <button
+                            onClick={toggleMute}
+                            className="absolute bottom-4 right-4 text-white bg-gray-800 rounded-full p-2 z-50"
+                            aria-label={
+                              isMuted ? "Ativar som" : "Desativar som"
                             }
-                          }}
-                        ></video>
-                        <button
-                          onClick={toggleMute}
-                          className="absolute bottom-4 right-4 text-white bg-gray-800 rounded-full p-2 z-50"
-                          aria-label={isMuted ? "Ativar som" : "Desativar som"}
-                        >
-                          {isMuted ? "🔇" : "🔊"}
-                        </button>
-                      </div>
-                    ) : (
-                      <Image
-                        width="1200"
-                        height="700"
-                        src={mediaItem.url}
-                        alt={mediaItem.description}
-                        className="w-full h-auto"
-                      />
-                    ))}
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+                          >
+                            {isMuted ? "🔇" : "🔊"}
+                          </button>
+                        </div>
+                      ) : (
+                        <Image
+                          width="1200"
+                          height="700"
+                          src={mediaItem.url}
+                          alt={mediaItem.description}
+                          className="max-h-[90vh] max-w-full object-contain"
+                        />
+                      ))}
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </Modal>
       )}
     </div>
