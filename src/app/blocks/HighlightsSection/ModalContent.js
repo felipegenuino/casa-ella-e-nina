dea84c1392aeea6a0318134b39a70b8ec1072dd0
Felipe Genuino <design@felipegenuino.com>
2024-12-02 05:01:22 -0300

    Titulos das galerias dinamicas com descricao

--- src/app/blocks/HighlightsSection/ModalContent.js
+++ src/app/blocks/HighlightsSection/ModalContent.js
@@ -7,17 +7,20 @@ import { handleSlideChange } from "./helpers";
 const ModalContent = ({ gallery, closeModal }) => {
   return (
     <div className="relative w-full h-full ">
+      {/* Header */}
       <div class="px-12 py-8  flex justify-between items-center ">
         <div>
           <h3 class="text-xl/7  font-semibold text-gray-100">
-            Galeria de Exemplo
+            {gallery.title || "Título da Galeria"}
           </h3>
-          <p class="mt-1 max-w-2xl text-sm/6 text-gray-500">Alguma descrição</p>
+          <p class="mt-1 max-w-2xl text-sm/6 text-gray-500">
+            {gallery.description || "Descrição da galeria"}
+          </p>
         </div>
 
         <button
           onClick={closeModal}
-          className="right-4 bg-gray-800 text-white flex flex-row content-center justify-between rounded-full align-center  px-4 py-2  z-50 gap-1 hover:bg-gray-900"
+          className="right-4 bg-gray-800 text-white flex items-center justify-between rounded-full px-4 py-2 z-50 gap-1 hover:bg-gray-900"
           aria-label="Fechar galeria"
         >
           <span className="text-sm">Fechar</span>
@@ -25,22 +28,21 @@ const ModalContent = ({ gallery, closeModal }) => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            aria-hidden="true"
-            data-slot="icon"
             className="w-5 h-5"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 18 18 6M6 6l12 12"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
             ></path>
           </svg>
         </button>
       </div>
 
-      <div className="_content-center h-full bg-slate-900 flex-1 py-12">
+      {/* Swiper Section */}
+      <div className=" h-full bg-slate-900 flex-1 py-12">
         <Swiper
           slidesPerView={1.4}
           centeredSlides
